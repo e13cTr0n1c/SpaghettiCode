5c0c441243efcf57edfeea8d399d4a532b0561e3
e13cTr0n1c <49526213+e13cTr0n1c@users.noreply.github.com>
2022-04-22 13:35:39 +0100

    update proccess now refreshes camera position immediately after player pos

--- Madeleine McCann Search Party.py
+++ Madeleine McCann Search Party.py
@@ -10,6 +10,8 @@ class Game:
         pg.init()
         #RESOLUTION = pg.display.Info()
         self.screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+        #uncomment line below for specific resolutions
+        #self.screen = pg.display.set_mode((1500, 1000))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         pg.key.set_repeat(500, 100)
@@ -23,6 +25,8 @@ class Game:
         self.tileheight = self.map.tileheight
         self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
         self.mob_img = pg.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
+        self.player_img= pg.transform.scale(self.player_img, (64, 64))
+        self.mob_img = pg.transform.scale(self.mob_img, (64, 64))
         self.wall_img = pg.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
         self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
 
@@ -31,7 +35,8 @@ class Game:
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
         self.mobs = pg.sprite.Group()
-        self.draw_grid()
+        self.camx, self.camy = 0, 0
+        #self.draw_grid()
 
         self.camera = Camera(self.map.width, self.map.height)
 
@@ -61,17 +66,17 @@ class Game:
 
     def update(self):
         # update portion of the game loop
-        self.camx, self.camy = self.camera.update(self.player)
-        self.all_sprites.update()
         #self.camx, self.camy = self.camera.update(self.player)
+        self.all_sprites.update()
+        self.camx, self.camy = self.camera.update(self.player)
 
     #UNCOMMENT THE FOLLOWING FOR GRID:
 
-    def draw_grid(self):
-        for x in range(0, WIDTH, TILESIZE):
-            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
-            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+    #def draw_grid(self):
+        #for x in range(0, WIDTH, TILESIZE):
+            #pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        #for y in range(0, HEIGHT, TILESIZE):
+            #pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
     def draw(self):
         pg.display.set_caption("{:.2f}".format(self.clock.get_fps()))
